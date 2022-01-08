e103bbb381250206fedeabc20b81807df50eae8a
PYKSOJ <6210110216@psu.ac.th>
2022-01-08 21:31:05 +0700

    first init

---
+++ src/pages/AboutMe.vue
@@ -0,0 +1,71 @@
+<template>
+  <q-page padding>
+    <div class="q-pa-md row items-start q-gutter-md">
+      <q-card
+        class="my-card text-white absulute-center"
+        flat
+        bordered
+        style="background: radial-gradient(circle, #35a2ff 0%, #014a88 100%)">
+        <q-card-section>
+          <div class="text-h5 q-mt-sm q-mb-xs">Our Changing Planet</div>
+          <div>
+            <q-item-label class="text-white" caption>
+              By John Doe
+            </q-item-label>
+          </div>
+        </q-card-section>
+        <q-card-section>
+          {{ lorem }}
+        </q-card-section>
+      </q-card>
+        <q-card class="my-card">
+      <q-card-section horizontal>
+        <q-img
+          class="col-5"
+          src="https://cdn.quasar.dev/img/parallax1.jpg"
+        />
+        <q-card-section>
+          {{ lorem }}
+        </q-card-section>
+      </q-card-section>
+      <q-separator />
+      <q-card-actions>
+        <q-btn flat round icon="event" />
+        <q-btn flat>
+          5:30PM
+        </q-btn>
+        <q-btn flat>
+          7:00PM
+        </q-btn>
+        <q-btn flat color="primary">
+          Reserve
+        </q-btn>
+      </q-card-actions>
+    </q-card>
+  </div>
+    <q-btn
+      class="absolute-center"
+      color="primary"
+      icon="home"
+      label="Go Home"
+      @click="$router.replace('/')"/>
+  </q-page>
+
+</template>
+
+<script>
+export default {
+  name: 'AboutMe',
+  setup () {
+    return {
+      lorem: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+    }
+  }
+}
+</script>
+
+<style lang="sass" scoped>
+.my-card
+  width: 100%
+  max-width: 350px
+</style>
---
+++ src/pages/Index.vue
@@ -0,0 +1,85 @@
+<template>
+  <q-page padding>
+    <div class="q-pa-md">
+      <div class="q-gutter-md">
+        <q-carousel
+          v-model="slide"
+          transition-prev="scale"
+          transition-next="scale"
+          swipeable
+          animated
+          control-color="white"
+          navigation
+          padding
+          arrows
+          height="300px"
+          class="bg-primary text-white shadow-1 rounded-borders"
+        >
+          <q-carousel-slide name="style" class="column no-wrap flex-center">
+            <q-page class="flex flex-center">
+              <img
+                alt="Quasar logo"
+                src="https://cdn.quasar.dev/img/mountains.jpg"
+                style="width: 100%; height: 39%"
+              />
+            </q-page>
+          </q-carousel-slide>
+          <q-carousel-slide name="tv" class="column no-wrap flex-center">
+            <q-page class="flex flex-center">
+              <img
+                alt="Quasar logo"
+                src="https://cdn.quasar.dev/img/mountains.jpg"
+                style="width: 100%; height: 50%"
+              />
+            </q-page>
+          </q-carousel-slide>
+          <q-carousel-slide name="layers" class="column no-wrap flex-center">
+            <q-page class="flex flex-center">
+              <img
+                alt="Quasar logo"
+                src="https://cdn.quasar.dev/img/mountains.jpg"
+                style="width: 100%; height: 65%"
+              />
+            </q-page>
+          </q-carousel-slide>
+          <q-carousel-slide name="map" class="column no-wrap flex-center">
+            <q-page class="flex flex-center">
+              <img
+                alt="Quasar logo"
+                src="https://cdn.quasar.dev/img/mountains.jpg"
+                style="width: 100%; height: 80%"
+              />
+            </q-page>
+          </q-carousel-slide>
+          <q-carousel-slide name="map" class="column no-wrap flex-center">
+            <q-page class="flex flex-center">
+              <img
+                alt="Quasar logo"
+                src="https://cdn.quasar.dev/img/mountains.jpg"
+                style="width: 100%; height: 80%"
+              />
+            </q-page>
+          </q-carousel-slide>
+        </q-carousel>
+      </div>
+      <q-btn
+        class="absolute-center"
+        color="primary"
+        icon="account_circle"
+        label="about me"
+        @click="$router.replace('/aboutme')"
+      />
+    </div>
+  </q-page>
+</template>
+<script>
+import { ref } from 'vue'
+
+export default {
+  setup () {
+    return {
+      slide: ref('style')
+    }
+  }
+}
+</script>
